e2dbb49e5db214ed737e6cb1b5bcc3c80ec11688
jxmine <jas9967765@gmail.com>
2021-06-14 10:38:08 +1200

    comments

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ from flask import Flask, g, render_template, request, redirect, session, url_for
 import sqlite3
 
 from flask.helpers import flash
+#importing all the things needed to run the website
 
 class User:
     def __init__(self, id, username, password):
@@ -12,34 +13,39 @@ class User:
     
     def __repr__(self):
         return f'<User: {self.username}>'
+        #shows the user's username instead of showing their id 
 
 users = []
 users.append(User(id=1, username = 'jasmine', password = 'hungry'))
 users.append(User(id=2, username = 'hanan', password = 'test'))
+#adds users
 
 app = Flask(__name__)
 DATABASE = 'todolist.db'
-app.secret_key = "jasmine"
 
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g.database = sqlite3.connect(DATABASE)
+        #gets the database
     return db
-#get database
+    #gets database
 
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, "_database", None)
     if db is not None:
         db.close()
-#close connection
+        #close connection
+
 @app.route("/")
 def home():
     if not g.user:
         return redirect(url_for("login"))
+        #if the user isn't logged in, redirects them to the login page
 
     return render_template("home.html")
+    #if the user is logged in, redirects the user to home
 
  
 @app.route("/Task_List")
@@ -49,6 +55,7 @@ def contents():
     SELECT Class.class, Task_List.task, To_Do_List.description, To_Do_List.due_date FROM To_Do_List 
     JOIN Task_List ON To_Do_List.task_id = Task_List.id
     JOIN Class ON To_Do_List.class_id = Class.id"""
+    #gets all the data from the database and show them as what they are instead of their ids
     cursor.execute(sql)
     results = cursor.fetchall()
     if not g.user:
@@ -87,6 +94,7 @@ def login():
             if user.username == username and user.password == password:
                 session['user_id'] = user.id
                 return redirect(url_for("home"))
+                #if the username and password is correct redirects to home
 
         flash("Failed to login")
         return redirect(url_for("login"))
@@ -96,6 +104,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop("user_id", None)
+    #removes the user from the session
     return redirect(url_for('home'))
 
 @app.route("/add", methods=["POST"])
@@ -122,6 +131,7 @@ def add_task():
     sql = "INSERT INTO To_Do_List(class_id, task_id, description, due_date) VALUES (?, ?, ?, ?)"
     print(klass_id, type(klass_id))
     cursor.execute(sql, (klass_id, task_id, description, new_due_date))
+    #gets the data from the database and puts each of the different data into different tables
     db.commit()
     return redirect("/Task_List")
 
